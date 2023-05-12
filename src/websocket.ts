76b90c2c2310b5392069ea94f9c22b7e20804761
Alisson Felipe <alissonfelipenettt@gmail.com>
2023-05-12 10:44:25 -0300

    feat: add a user's connection to a room

--- src/websocket.ts
+++ src/websocket.ts
@@ -1,5 +1,29 @@
 import { io } from "./http";
 
+interface RoomUser {
+  socketId: string;
+  username: string;
+  room: string;
+}
+
+const users: RoomUser[] = [];
+
 io.on("connection", (socket) => {
-  console.log(`User connected on socket ${socket.id}`);
+  socket.on("select_room", (data) => {
+    socket.join(data.room);
+
+    const userInRoom = users.find(
+      (user) => user.username === data.username && user.room === data.room
+    );
+
+    if (userInRoom) {
+      userInRoom.socketId = socket.id;
+    } else {
+      users.push({
+        socketId: socket.id,
+        username: data.username,
+        room: data.room,
+      });
+    }
+  });
 });
